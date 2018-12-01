87c79d49daf79a1fdfbcf47c42ced8802a009164
fizza <fizza@DESKTOP-083P1OV.attlocal.net>
2018-11-30 17:57:25 -0800

    Window with tabs and menu bar

---
+++ CSC131-Project/src/GUIconstruction.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class GUIconstruction {
+	public static void main(String[]args) {
+		
+		JFrame       frame = new JFrame("Pirex");
+		Container    contentPane = frame.getContentPane();
+		JPanel       search, load, summarize;
+		JTabbedPane  tabbedPane;
+		JMenuBar     menuBar;
+		JMenu        helpMenu, optionsMenu; 
+		//JMenuItem    item; Can add items inside the help and options in menu
+
+		//create and add the menu bar to the frame
+		menuBar     = new JMenuBar();
+		helpMenu    = new JMenu("Help");
+		optionsMenu = new JMenu("Options");
+		menuBar.add(helpMenu);
+		menuBar.add(optionsMenu);
+		frame.setJMenuBar(menuBar);
+
+		//Create the three tabs 
+		tabbedPane = new JTabbedPane();
+		contentPane.setLayout(new BorderLayout());
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
+
+		search = new JPanel();
+		//search.setLayout(new BorderLayout()); Uncomment later for changes (choose a layout for each tab)
+		tabbedPane.addTab("Search For documents", search);
+
+
+		load = new JPanel();
+		// load.setLayout(new BorderLayout());Uncomment later for changes (choose a layout for each tab)
+		tabbedPane.addTab("Load Documents",  load);
+
+		 
+		 summarize = new JPanel();
+		// summarize.setLayout(new BorderLayout());Uncomment later for changes (choose a layout for each tab)
+		 tabbedPane.addTab("Summarize Documents",  summarize);
+
+		 //The current frame is set to these 
+		 frame.setSize(800,600);
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 frame.setVisible(true);
+		 frame.setResizable(true); 
+	
+	}
+	
+	
+}
