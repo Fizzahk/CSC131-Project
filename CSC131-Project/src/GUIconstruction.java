8397609466e907918e41baad5b5aaac3a0bf8afd
Pawan Khatri (PK) <pawankhatri2@csus.edu>
2018-12-04 15:25:54 -0800

    Add File Menu to the Menu Bar

--- CSC131-Project/src/GUIconstruction.java
+++ CSC131-Project/src/GUIconstruction.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
 public class GUIconstruction {
 	
 	public static void main(String[]args) {
@@ -12,57 +11,62 @@ public class GUIconstruction {
 		JPanel       search, load, summarize;
 		JTabbedPane  tabbedPane;
 		JMenuBar     menuBar;
-		JMenu        helpMenu, optionsMenu; 
+		JMenu        helpMenu, optionsMenu, fileMenu;
 		JScrollPane  scrollpane;
 		JMenuItem    menuItem;; //Can add items inside the help and options in menu
 
-		//create and add the menu bar to the frame
+		//Creates and Adds a menu bar to the Pirex Frame
 		menuBar     = new JMenuBar();
+		fileMenu 	= new JMenu("File"); //Creates file Menu to the Menu Bar - Pawan (PK) Khatri
 		helpMenu    = new JMenu("Help");
 		optionsMenu = new JMenu("Options");
+		
+		menuBar.add(fileMenu); //Adds file Menu to the Menu Bar - Pawan (PK) Khatri
 		menuBar.add(helpMenu);
 		menuBar.add(optionsMenu);
 		JMenuItem optionsMenu11 = new JMenuItem("Documents");
 		optionsMenu.add(optionsMenu11);
 		frame.setJMenuBar(menuBar);
 		
-		// help menu item
+		//Creates Help Menu Item
 	    menuItem = new JMenuItem("About",KeyEvent.VK_T);
 	    helpMenu.add(menuItem);
 	      
+	    //Adds an action listener to the Help Menu Item
 	    menuItem.addActionListener(new ActionListener(){  
 	    public void actionPerformed(ActionEvent e){ 
 	    JLabel label = new JLabel("Pirex - (Personal Information Retrieval Experimental System) is an information retrieval system that individuals can use to investigate their own texts..");
 	    JOptionPane.showMessageDialog(null,label,"About",JOptionPane.INFORMATION_MESSAGE);
-	    }}); 
-	    
+	    }});    
  
 		//Create the three tabs 
 		tabbedPane = new JTabbedPane();
 		contentPane.setLayout(new BorderLayout());
 		contentPane.add(tabbedPane, BorderLayout.CENTER);
 
+		//Creates Search Tab
 		search = new JPanel();
 		//search.setLayout(new BorderLayout()); Uncomment later for changes (choose a layout for each tab)
 		tabbedPane.addTab("Search For Documents", search);
-		frame.getContentPane().setLayout(new FlowLayout());
-		frame.getContentPane().setLayout(new FlowLayout());
-
+		
+	    
+		//Creates Load Tab
 		load = new JPanel();
 		// load.setLayout(new BorderLayout());Uncomment later for changes (choose a layout for each tab)
 		tabbedPane.addTab("Load Documents",  load);
-		 
+		
+		//Creates Summary Tab
 		summarize = new JPanel();
 		summarize.setLayout(new BorderLayout());
 		tabbedPane.addTab("Summarize Documents",  summarize);
 		scrollpane = new JScrollPane();
 	    summarize.add(scrollpane, BorderLayout.CENTER);
-	    
-		 //The current frame is set to these 
-		 frame.setSize(800,600);
-		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 frame.setVisible(true);
-		 frame.setResizable(true); 
-			 
+
+		//The current frame is set to these 
+	    frame.setSize(800,600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setResizable(true); 
+	
 	}
 }
